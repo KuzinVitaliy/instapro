012bdd6e7ac485c66e0bf1dbf4ff809a754536f3
KuzinVitaliy <vkuzin@vk-soft.ru>
2025-03-07 16:29:02 +0300

    ss

---
+++ 1.bat
@@ -0,0 +1,8 @@
+echo "# WebApp" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/KuzinVitaliy/instapro.git
+git push -u origin main
+
